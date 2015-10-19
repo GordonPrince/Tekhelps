4442aa102296294a21f0816fb0a448add89f41eb
Gordon Prince <gordon.prince@tekhelps.onmicrosoft.com>
2015-10-19 17:41:07 -0300

    Added first version to GIT

---
+++ GKBMOutlook/AddinModule.Designer.vb
@@ -0,0 +1,133 @@
+﻿Imports System.Windows.Forms
+
+Partial Public Class AddinModule
+
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Public Sub New()
+        MyBase.New()
+
+        Application.EnableVisualStyles()
+
+        'This call is required by the Component Designer
+        InitializeComponent()
+
+        'Please add any initialization code to the AddinInitialize event handler
+
+    End Sub
+
+    'Component overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Component Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Component Designer
+    'It can be modified using the Component Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.AdxRibbonTab1 = New AddinExpress.MSO.ADXRibbonTab(Me.components)
+        Me.AdxRibbonGroup1 = New AddinExpress.MSO.ADXRibbonGroup(Me.components)
+        Me.CopyContact2InstantFile = New AddinExpress.MSO.ADXRibbonButton(Me.components)
+        Me.AdxRibbonButton2 = New AddinExpress.MSO.ADXRibbonButton(Me.components)
+        Me.AdxRibbonButtonSaveAttachments = New AddinExpress.MSO.ADXRibbonButton(Me.components)
+        Me.AdxRibbonButton1 = New AddinExpress.MSO.ADXRibbonButton(Me.components)
+        Me.AdxRibbonButton4 = New AddinExpress.MSO.ADXRibbonButton(Me.components)
+        '
+        'AdxRibbonTab1
+        '
+        Me.AdxRibbonTab1.Caption = "Tekhelps"
+        Me.AdxRibbonTab1.Controls.Add(Me.AdxRibbonGroup1)
+        Me.AdxRibbonTab1.Id = "adxRibbonTab_a5cf84a1ced7485ea57b9386db6cf9ed"
+        Me.AdxRibbonTab1.Ribbons = CType(((((AddinExpress.MSO.ADXRibbons.msrOutlookMailRead Or AddinExpress.MSO.ADXRibbons.msrOutlookMailCompose) _
+            Or AddinExpress.MSO.ADXRibbons.msrOutlookContact) _
+            Or AddinExpress.MSO.ADXRibbons.msrOutlookTask) _
+            Or AddinExpress.MSO.ADXRibbons.msrOutlookExplorer), AddinExpress.MSO.ADXRibbons)
+        '
+        'AdxRibbonGroup1
+        '
+        Me.AdxRibbonGroup1.Caption = "InstantFile"
+        Me.AdxRibbonGroup1.Controls.Add(Me.CopyContact2InstantFile)
+        Me.AdxRibbonGroup1.Controls.Add(Me.AdxRibbonButton2)
+        Me.AdxRibbonGroup1.Controls.Add(Me.AdxRibbonButtonSaveAttachments)
+        Me.AdxRibbonGroup1.Controls.Add(Me.AdxRibbonButton1)
+        Me.AdxRibbonGroup1.Controls.Add(Me.AdxRibbonButton4)
+        Me.AdxRibbonGroup1.Id = "adxRibbonGroup_072621b7e27f4dc6966496c4216ab446"
+        Me.AdxRibbonGroup1.ImageTransparentColor = System.Drawing.Color.Transparent
+        Me.AdxRibbonGroup1.Ribbons = CType(((((AddinExpress.MSO.ADXRibbons.msrOutlookMailRead Or AddinExpress.MSO.ADXRibbons.msrOutlookMailCompose) _
+            Or AddinExpress.MSO.ADXRibbons.msrOutlookContact) _
+            Or AddinExpress.MSO.ADXRibbons.msrOutlookTask) _
+            Or AddinExpress.MSO.ADXRibbons.msrOutlookExplorer), AddinExpress.MSO.ADXRibbons)
+        '
+        'CopyContact2InstantFile
+        '
+        Me.CopyContact2InstantFile.Caption = "Copy Contact to InstantFile"
+        Me.CopyContact2InstantFile.Id = "adxRibbonButton_0e8a15c449ee4798adf475a1587fd047"
+        Me.CopyContact2InstantFile.ImageTransparentColor = System.Drawing.Color.Transparent
+        Me.CopyContact2InstantFile.Ribbons = CType((AddinExpress.MSO.ADXRibbons.msrOutlookContact Or AddinExpress.MSO.ADXRibbons.msrOutlookExplorer), AddinExpress.MSO.ADXRibbons)
+        Me.CopyContact2InstantFile.ScreenTip = "Copy this personal Contact to InstantFile's Contacts"
+        '
+        'AdxRibbonButton2
+        '
+        Me.AdxRibbonButton2.Caption = "Link Two Contacts"
+        Me.AdxRibbonButton2.Id = "adxRibbonButton_4b812ad786fc4beb9f6a39f3bbcb0352"
+        Me.AdxRibbonButton2.ImageTransparentColor = System.Drawing.Color.Transparent
+        Me.AdxRibbonButton2.Ribbons = AddinExpress.MSO.ADXRibbons.msrOutlookContact
+        Me.AdxRibbonButton2.ScreenTip = "Link two Contacts to each other."
+        '
+        'AdxRibbonButtonSaveAttachments
+        '
+        Me.AdxRibbonButtonSaveAttachments.Caption = "Save Attachments"
+        Me.AdxRibbonButtonSaveAttachments.Id = "adxRibbonButton_4c9aad6ef1324d11bf940cb9a7ca7623"
+        Me.AdxRibbonButtonSaveAttachments.ImageTransparentColor = System.Drawing.Color.Transparent
+        Me.AdxRibbonButtonSaveAttachments.Ribbons = CType(((AddinExpress.MSO.ADXRibbons.msrOutlookMailRead Or AddinExpress.MSO.ADXRibbons.msrOutlookPostRead) _
+            Or AddinExpress.MSO.ADXRibbons.msrOutlookMMSRead), AddinExpress.MSO.ADXRibbons)
+        Me.AdxRibbonButtonSaveAttachments.ScreenTip = "Save Attachments to C:\Scans folder."
+        '
+        'AdxRibbonButton1
+        '
+        Me.AdxRibbonButton1.Caption = "Copy to Folder"
+        Me.AdxRibbonButton1.Id = "adxRibbonButton_de572db5435f4b35bc780bc9d332c327"
+        Me.AdxRibbonButton1.ImageTransparentColor = System.Drawing.Color.Transparent
+        Me.AdxRibbonButton1.Ribbons = CType((AddinExpress.MSO.ADXRibbons.msrOutlookTask Or AddinExpress.MSO.ADXRibbons.msrOutlookExplorer), AddinExpress.MSO.ADXRibbons)
+        Me.AdxRibbonButton1.ScreenTip = "Copy this item to another folder."
+        '
+        'AdxRibbonButton4
+        '
+        Me.AdxRibbonButton4.Caption = "About"
+        Me.AdxRibbonButton4.Id = "adxRibbonButton_54403c6dbcd54328a298f2556650e611"
+        Me.AdxRibbonButton4.ImageTransparentColor = System.Drawing.Color.Transparent
+        Me.AdxRibbonButton4.Ribbons = CType((((((((AddinExpress.MSO.ADXRibbons.msrOutlookMailRead Or AddinExpress.MSO.ADXRibbons.msrOutlookMeetingRequestRead) _
+            Or AddinExpress.MSO.ADXRibbons.msrOutlookAppointment) _
+            Or AddinExpress.MSO.ADXRibbons.msrOutlookContact) _
+            Or AddinExpress.MSO.ADXRibbons.msrOutlookTask) _
+            Or AddinExpress.MSO.ADXRibbons.msrOutlookPostRead) _
+            Or AddinExpress.MSO.ADXRibbons.msrOutlookExplorer) _
+            Or AddinExpress.MSO.ADXRibbons.msrOutlookMMSRead), AddinExpress.MSO.ADXRibbons)
+        '
+        'AddinModule
+        '
+        Me.AddinName = "GKBMOutlook"
+        Me.SupportedApps = AddinExpress.MSO.ADXOfficeHostApp.ohaOutlook
+
+    End Sub
+    Private WithEvents AdxRibbonTab1 As AddinExpress.MSO.ADXRibbonTab
+    Friend WithEvents AdxRibbonGroup1 As AddinExpress.MSO.ADXRibbonGroup
+    Friend WithEvents CopyContact2InstantFile As AddinExpress.MSO.ADXRibbonButton
+    Friend WithEvents AdxRibbonButton2 As AddinExpress.MSO.ADXRibbonButton
+    Friend WithEvents AdxRibbonButtonSaveAttachments As AddinExpress.MSO.ADXRibbonButton
+    Friend WithEvents AdxRibbonButton1 As AddinExpress.MSO.ADXRibbonButton
+    Friend WithEvents AdxRibbonButton4 As AddinExpress.MSO.ADXRibbonButton
+
+End Class
+
---
+++ GKBMOutlook/AddinModule.vb
@@ -0,0 +1,117 @@
+﻿Imports System.Runtime.InteropServices
+Imports System.ComponentModel
+Imports System.Windows.Forms
+Imports AddinExpress.MSO
+Imports Outlook = Microsoft.Office.Interop.Outlook
+
+'Add-in Express Add-in Module
+<GuidAttribute("7E29F01B-BDC1-47B5-B1B7-634B70EA309B"), ProgIdAttribute("GKBMOutlook.AddinModule")> _
+Public Class AddinModule
+    Inherits AddinExpress.MSO.ADXAddinModule
+
+#Region " Add-in Express automatic code "
+ 
+    'Required by Add-in Express - do not modify
+    'the methods within this region
+ 
+    Public Overrides Function GetContainer() As System.ComponentModel.IContainer
+        If components Is Nothing Then
+            components = New System.ComponentModel.Container
+        End If
+        GetContainer = components
+    End Function
+ 
+    <ComRegisterFunctionAttribute()> _
+    Public Shared Sub AddinRegister(ByVal t As Type)
+        AddinExpress.MSO.ADXAddinModule.ADXRegister(t)
+    End Sub
+ 
+    <ComUnregisterFunctionAttribute()> _
+    Public Shared Sub AddinUnregister(ByVal t As Type)
+        AddinExpress.MSO.ADXAddinModule.ADXUnregister(t)
+    End Sub
+ 
+    Public Overrides Sub UninstallControls()
+        MyBase.UninstallControls()
+    End Sub
+ 
+#End Region
+ 
+    Public Shared Shadows ReadOnly Property CurrentInstance() As AddinModule
+        Get
+            Return CType(AddinExpress.MSO.ADXAddinModule.CurrentInstance, AddinModule)
+        End Get
+    End Property
+
+    Public ReadOnly Property OutlookApp() As Outlook._Application
+        Get
+            Return CType(HostApplication, Outlook._Application)
+        End Get
+    End Property
+
+    Private Sub AdxRibbonButton4_OnClick(sender As Object, control As IRibbonControl, pressed As Boolean) Handles AdxRibbonButton4.OnClick
+        MsgBox("Tekhelps Add-in for GKBM Outlook" & vbNewLine & vbNewLine & "Version 2015-Oct-17", vbInformation, "About")
+    End Sub
+
+    Private Sub AdxRibbonButtonSaveAttachments_OnClick(sender As Object, control As IRibbonControl, pressed As Boolean) Handles AdxRibbonButtonSaveAttachments.OnClick
+        ' copied from http://www.howto-outlook.com/howto/saveembeddedpictures.htm
+        Const strTitle As String = "Save Attachments"
+        Dim myOlNameSpace As Outlook.NameSpace, myOlSelection As Outlook.Selection
+        Dim mySelectedItem As Object, intPos As Integer
+        Dim colAttachments As Outlook.Attachments, objAttachment As Outlook.Attachment
+        Dim DateStamp As String, MyFile As String
+        Dim intCounter As Integer
+        Dim RetVal As VariantType
+
+        'Get all selected items
+        myOlNameSpace = OutlookApp.GetNamespace("MAPI")
+        myOlSelection = OutlookApp.ActiveExplorer.Selection
+        'Make sure at least one item is selected
+        If myOlSelection.Count = 0 Then
+            RetVal = MsgBox("Please select an item first.", vbExclamation, strTitle)
+            Exit Sub
+        End If
+
+        'Make sure only one item is selected
+        If myOlSelection.Count > 1 Then
+            RetVal = MsgBox("Please select only one item.", vbExclamation, strTitle)
+            Exit Sub
+        End If
+
+        'Retrieve the selected item
+        mySelectedItem = myOlSelection.Item(1)
+
+        'Retrieve all attachments from the selected item
+        colAttachments = mySelectedItem.Attachments
+
+        'Save all attachments to the selected location with a date and time stamp of message to generate a unique name
+        For Each objAttachment In colAttachments
+            If objAttachment.Size > 15000 Then
+                MyFile = objAttachment.FileName
+                DateStamp = Space(1) & Format(mySelectedItem.CreationTime, "yyyymmdd_hhnnss")
+                intPos = InStrRev(MyFile, ".")
+                If intPos > 0 Then
+                    MyFile = Left(MyFile, intPos - 1) & DateStamp & Mid(MyFile, intPos)
+                Else
+                    MyFile = MyFile & DateStamp
+                End If
+                MyFile = "C:\Scans\" & MyFile
+                objAttachment.SaveAsFile(MyFile)
+                intCounter = intCounter + 1
+                If intCounter = 1 Then MsgBox("Saved attachment " & IIf(intCounter > 1, "#" & intCounter & Space(1), vbNullString) & "as" & vbNewLine & MyFile, vbInformation, strTitle)
+            End If
+        Next
+        'Cleanup
+        objAttachment = Nothing
+        colAttachments = Nothing
+        myOlNameSpace = Nothing
+        myOlSelection = Nothing
+        mySelectedItem = Nothing
+        If intCounter = 0 Then
+            MsgBox("There are no attachments on this item larger than 15k.", vbInformation, strTitle)
+        ElseIf intCounter > 1 Then
+            MsgBox("Saved " & intCounter - 1 & " additional attachment" & IIf(intCounter = 2, vbNullString, "s") & " to " & vbNewLine & "'C:\Scans\' folder.", vbInformation, strTitle)
+        End If
+    End Sub
+End Class
+
