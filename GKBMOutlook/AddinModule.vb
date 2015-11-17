10357bd69ee05f96d9b8bc0026e3dadacab209ed
Gordon Prince <gordon.prince@tekhelps.onmicrosoft.com>
2015-11-17 08:46:40 -0400

    ready for testing, then hopefully deployment

--- GKBMOutlook/AddinModule.vb
+++ GKBMOutlook/AddinModule.vb
@@ -364,7 +364,7 @@ HaveNewCallTracking:
                "Gatti, Keltner, Bienvenu & Montesi, PLC." & vbNewLine & vbNewLine & _
                "Copyright (c) 1997-2015 by Tekhelps, Inc." & vbNewLine & _
                "For further information contact Gordon Prince (901) 761-3393." & vbNewLine & vbNewLine & _
-               "This version dated 2015-Nov-16 14:00.", vbInformation, "About this Add-in")
+               "This version dated 2015-Nov-17  8:45.", vbInformation, "About this Add-in")
     End Sub
 
     Private Sub SaveAttachments_OnClick(sender As Object, control As IRibbonControl, pressed As Boolean) Handles AdxRibbonButtonSaveAttachments.OnClick
